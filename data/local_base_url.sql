ad0caf9a48b1de5913825f7b67de0dc3eff89d90
root <root@gmail.com>
2017-02-27 20:15:54 +0000

    dd

---
+++ data/local_base_url.sql
@@ -0,0 +1,1 @@
+update core_config_data set value = 'http://local.vif.com/' where path like '%base_url%';
---
+++ data/production_base_url.sql
@@ -0,0 +1,2 @@
+update core_config_data set value = 'http://vif.comandas.com.ar/' where path like '%base_url%';
+
